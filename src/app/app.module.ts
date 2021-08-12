0b045ba57a830e7261fb8e29b7c031f9109d1b3b
tss-legal-code <tss.legal.code@gmail.com>
2021-08-12 15:06:19 +0300

    displaying and manipulating heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeroesComponent} from './heroes/heroes.component';
+import {Hero} from "./hero";
+import {FormsModule} from "@angular/forms"
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,20 @@
+/**
+ * Created by nitro on 11.08.2021.
+ */
+
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Lorem' },
+  { id: 12, name: 'Ipsum' },
+  { id: 13, name: 'Dolor' },
+  { id: 14, name: 'Sit' },
+  { id: 15, name: 'Amet' },
+  { id: 16, name: 'Consectetur' },
+  { id: 17, name: 'Adipisicing' },
+  { id: 18, name: 'Elit' },
+  { id: 19, name: 'Doloribus' },
+  { id: 20, name: 'Vel' }
+];
+
+
